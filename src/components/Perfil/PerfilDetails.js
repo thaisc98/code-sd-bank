db4d1d9e0aa22fcb07337d621cc5d95027ffbe87
Fernando0612 <fernandocabrera0612@gmail.com>
2020-10-07 21:24:46 -0400

    [check] check is not searching

---
+++ src/components/Perfil/PerfilDetails.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { fetchPerfilById } from "../../state-mgmt/actions/perfil.actions";
+import { getReadibleDate } from "../../utils/date-formatter";
+import { Button, Descriptions } from "antd";
+import { Link } from "react-router-dom";
+import { LeftOutlined } from "@ant-design/icons";
+
+const PerfilDetails = ({ match, perfilActual, fetchPerfilById }) => {
+  useEffect(() => {
+    const init = async () => {
+      await fetchPerfilById(match.params._id);
+    };
+    init();
+
+    console.log('perfilA',perfilActual)
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <Link to="/perfiles">
+        <Button className="mb-4">
+          <LeftOutlined />
+        </Button>
+      </Link>
+      <h3>Detalles del Perfil</h3>
+      {perfilActual && (
+        <Descriptions bordered layout="vertical" column={{ xxl: 4, xl: 4 }}>
+          <Descriptions.Item label="Rol">
+            {perfilActual.rol}
+          </Descriptions.Item>
+          <Descriptions.Item label="Descripcion">
+            {perfilActual.descripcion}
+          </Descriptions.Item>
+          <Descriptions.Item label="Entidad Asociada">
+            {perfilActual.tipo_entidad_asociada}
+          </Descriptions.Item>
+          <Descriptions.Item label="Creado en">
+            {getReadibleDate(perfilActual.createdAt)}
+          </Descriptions.Item>
+          <Descriptions.Item label="Última actualización">
+            {getReadibleDate(perfilActual.createdAt)}
+          </Descriptions.Item>
+        </Descriptions>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  perfilActual: state.perfiles.perfilActual,
+});
+
+export default connect(mapStateToProps, {
+  fetchPerfilById,
+})(PerfilDetails);
